6be3170746b36ad04855e5cac9a63720066e51a0
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-03-13 19:26:33 +0100

    refactor(dapp): added type definition for useSpaceAvailabilityHook

--- packages/dapp/src/hooks/useSpaceAvailability.ts
+++ packages/dapp/src/hooks/useSpaceAvailability.ts
@@ -1,11 +1,20 @@
-import {useContract} from "./useContract";
-import {useAppState} from "../store";
-import {useCallback, useState, useEffect} from "react";
+import { useContract } from './useContract';
+import { useAppState } from '../store';
+import { useCallback, useState, useEffect } from 'react';
 
-export const useSpaceAvailability = () => {
-  const {provider, ipfsNode, networkId} = useAppState();
+export type UseSpaceAvailabilityHook = [
+  cb: (
+    spaceId: string,
+    startDay: number,
+    numberOfDays: number
+  ) => Promise<number[]>,
+  isReady: boolean
+];
+
+// This hook provides a callback function for getting a space availability
+export const useSpaceAvailability = (): UseSpaceAvailabilityHook => {
+  const { provider, ipfsNode, networkId } = useAppState();
   const [contract] = useContract(provider, ipfsNode, networkId);
-
   const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
