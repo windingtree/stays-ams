c3279125a6b08177c59618ec56791b196eb211c5
bobetbat <b.bobetayev.97@gmail.com>
2022-03-22 09:05:50 +0100

    fixup

--- packages/dapp/src/components/buttons/BookWithDai.tsx
+++ packages/dapp/src/components/buttons/BookWithDai.tsx
@@ -1,33 +1,26 @@
-import { useContext } from 'react';
-import { Button, Box, Spinner, Text, ResponsiveContext } from 'grommet';
-import { Login, Logout } from 'grommet-icons';
+import { Button, Text, Spinner, Box } from 'grommet';
 import styled from 'styled-components';
-import { useAppState } from '../../store';
-import { useBookSpace } from '../../hooks/useBookSpace';
 
 const InnerSpinner = styled(Spinner)`
   margin-left: 8px;
 `;
 
-export const BookWithDai: React.FC<{ spaceId: string }> = ({ spaceId }) => {
-  const size = useContext(ResponsiveContext);
-  const { searchParams, signIn } = useAppState();
-  const [cb, isReady, error] = useBookSpace();
+export const BookWithDai: React.FC<{
+  loading: boolean; disabled: boolean; onClick: () => void
+}> = ({ loading, disabled, onClick }) => {
 
   return (
-    searchParams !== undefined ?
-      <Button
-        onClick={() => cb(
-          spaceId,
-          searchParams.startDay,
-          searchParams.numberOfDays,
-          searchParams.guestsAmount
-        )}
-        label='Book with DAI'
-      /> :
-      <Button
-        disabled={true}
-        label='Book with DAI'
-      />
+    <Button
+      onClick={() => onClick()}
+      disabled={disabled}
+    >
+      {() => (
+        <Box direction='row' align='center' pad='small'>
+          <Text>
+            Book with DAI
+          </Text>
+          {loading && <InnerSpinner />}
+        </Box>)}
+    </Button>
   )
 };
--- packages/dapp/src/pages/Space.tsx
+++ packages/dapp/src/pages/Space.tsx
@@ -1,15 +1,66 @@
 import { PageWrapper } from './PageWrapper';
-import { Tag, Box, Text, Button, Image, Carousel } from 'grommet';
+import { Tag, Box, Text, Image, Carousel, Spinner } from 'grommet';
 import { useAppState } from '../store';
-import { useMemo } from 'react';
+import { useMemo, useEffect, useState } from 'react';
 import { ThemeMode } from '../components/SwitchThemeMode';
 import { BookWithDai } from '../components/buttons/BookWithDai';
+import { SignInButton } from '../components/buttons/web3Modal';
+import { MessageBox } from '../components/MessageBox';
+import { ExternalLink } from '../components/ExternalLink';
+import { getNetwork } from '../config';
+import { centerEllipsis } from '../utils/strings';
+import { useContract } from '../hooks/useContract';
+import { NavLink } from 'react-router-dom';
 
 export const Space: React.FC = () => {
-  const { searchSpaces, themeMode } = useAppState();
-  const searchParams = window.location.pathname.substring(7)
+  const { account, searchSpaces, themeMode, searchParams, provider, ipfsNode } = useAppState();
+  const query = window.location.pathname.substring(7)
+  const space = useMemo(() => searchSpaces.find((space) => space.id === query), [searchSpaces, query])
 
-  const space = useMemo(() => searchSpaces.find((space) => space.id === searchParams), [searchSpaces, searchParams])
+  const [contract, , errorContract] = useContract(provider, ipfsNode);
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>();
+  const [hash, setHash] = useState('');
+  const [tokenId, setTokenId] = useState<string | undefined>();
+
+  useEffect(() => {
+    setError(errorContract);
+  }, [errorContract]);
+
+  const hashLink = useMemo(() => {
+    const network = getNetwork()
+    return hash ? `${network.blockExplorer}/tx/${hash}` : null
+  }, [hash])
+
+  const handler = async () => {
+    try {
+      setLoading(true)
+      if (!contract) {
+        throw new Error('Contract is not connected');
+      }
+      if (searchParams === undefined) {
+        throw new Error('searchParams is undefined')
+      }
+      if (space === undefined) {
+        throw new Error('space is undefined')
+      }
+      setError(undefined);
+
+      const res = await contract.book(
+        space.id,
+        searchParams.startDay,
+        searchParams.numberOfDays,
+        searchParams.guestsAmount,
+        undefined,
+        setHash
+      )
+      setTokenId(res)
+      setLoading(false)
+    } catch (error) {
+      setError((error as Error).message);
+    }
+  }
 
   const borderColor = themeMode === ThemeMode.light ? 'brand' : 'accent-1'
   return (
@@ -20,6 +71,21 @@ export const Space: React.FC = () => {
         }
       ]}
     >
+      <MessageBox type='info' show={!!tokenId}>
+        <Box direction='row'>
+          <Box>
+            Booked succesfully!
+            <NavLink to={`/tokens/${tokenId}`}> Check details </NavLink>
+          </Box>
+        </Box>
+      </MessageBox>
+      <MessageBox type='error' show={!!error}>
+        <Box direction='row'>
+          <Box>
+            {error}
+          </Box>
+        </Box>
+      </MessageBox>
       {space === undefined ? <>Space not found</> : <Box
         border={{ color: borderColor }}
         flex={true}
@@ -42,8 +108,9 @@ export const Space: React.FC = () => {
         </Box>
         <Box width='100%' align='center' gridArea="img" height="xlarge" pad={{ bottom: 'medium' }}>
           <Carousel height='xxlarge' width='xlarge'>
-            {space.media.images?.map((space) =>
+            {space.media.images?.map((space, i) =>
               <Image
+                key={i}
                 width='xlarge'
                 fit="cover"
                 alignSelf='center'
@@ -73,7 +140,19 @@ export const Space: React.FC = () => {
         </Box>
         <Box pad={{ right: 'medium' }} direction='row' width='100%' justify='between' align='center' gridArea="action">
           <Text>Price per Night: <Text color={borderColor} size='large'>{parseInt(`${space.pricePerNightWei}`)} DAI</Text></Text>
-          <BookWithDai spaceId={space.id} />
+          <Box align='center'>
+            {account ? <BookWithDai
+              onClick={handler}
+              loading={loading}
+              disabled={!!error || !!tokenId}
+            />
+              : <SignInButton />
+            }
+            {hashLink !== null ?
+              <ExternalLink href={hashLink} label={centerEllipsis(hash)} />
+              : null
+            }
+          </Box>
         </Box>
       </Box>
       }
