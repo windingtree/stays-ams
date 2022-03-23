52d1dac702e3340225cc616c35448f35d2cb84af
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-03-23 16:11:02 +0100

    feat(dapp): add useWeb3StorageApi hook

---
+++ packages/dapp/src/hooks/useWeb3StorgaeApi.ts
@@ -0,0 +1,13 @@
+import type { IPFS } from '@windingtree/ipfs-apis';
+import { useMemo } from 'react';
+import { Web3StorageApi } from '@windingtree/ipfs-apis';
+import { getApiKey } from '../config';
+
+export const useWeb3StorageApi = (
+  ipfsNode: IPFS
+): Web3StorageApi | undefined => useMemo(
+  () => !!ipfsNode
+    ? new Web3StorageApi(getApiKey('web3Storage'), ipfsNode)
+    : undefined,
+  [ipfsNode]
+);
