4c074cfbddf1a475e6c7aa78c34a28fbd27b7f29
mfw78 <mfw78@chasingseed.com>
2022-04-12 13:42:42 +0000

    fix: wrong smart contract license

--- packages/smart-contracts/contracts/Stays.sol
+++ packages/smart-contracts/contracts/Stays.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL
+// SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
