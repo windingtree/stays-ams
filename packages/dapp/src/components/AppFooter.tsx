1df5b381a477e65a901dbbd2d433948b2840de6c
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-04-14 10:15:10 +0200

    refactor(dapp): "created with" moved to the footer

--- packages/dapp/src/components/AppFooter.tsx
+++ packages/dapp/src/components/AppFooter.tsx
@@ -1,18 +1,36 @@
-import { Image, Box, Footer, Anchor } from 'grommet';
-
+import { Image, Text, Box, Footer, Anchor } from 'grommet';
 
 export const AppFooter = () => {
   return (
     <Footer
-      pad='small'
       responsive={true}
-      border='top'
       justify='between'
+      margin={{ left: 'auto', right: 'auto', bottom: 'xlarge' }}
+      pad={{ horizontal: 'small'}}
+      width={{ width: '100%', max: '900px' }}
+      direction='column'
     >
-      <Box>
+      <Box align='center' margin={{ bottom: 'large' }}>
+        <Text size='large'>
+          Created with 💖 for <a href="https://devconnect.org/">Devconnect</a>
+        </Text>
+        <Text size='large'>
+          April 17&ndash;25, Amsterdam
+        </Text>
       </Box>
 
-      <Box>
+      <Box align='center'>
+        <a href="https://windingtree.com">
+          <Image
+            style={{ height: '32px' }}
+            src='https://raw.githubusercontent.com/windingtree/branding/master/winding-tree/svg/winding-tree-symbol-dark.svg'
+            alt='Powered by Winding Tree'
+            title='Powered by Winding Tree'
+          />
+        </a>
+      </Box>
+
+      <Box align='center'>
         <Anchor
           icon={<Image src='/discord-icon.png' height='20px' />}
           href='https://discord.gg/RWqqzT3Gf8'
