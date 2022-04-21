f653b47ae2ee503d53de981e9564543f5841141c
rans <Galeaf11@gmail.com>
2022-04-21 21:08:08 +0600

    WIP: Stays Watcher

--- packages/watcher/test/package.spec.ts
+++ packages/watcher/test/package.spec.ts
@@ -2,23 +2,4 @@ import * as index from '../src/index'
 import {expect} from 'chai';
 
 describe('testing', () => {
-
-  it('test', async () => {
-    const a = Math.floor(Math.random() * 100);
-    const b = Math.floor(Math.random() * 100);
-    const result = index.testing(a, b);
-    expect(result).to.be.an('number');
-    expect(result).to.equal(a + b);
-  });
-
-  it('event service test', async () => {
-    const [result, sent] = index.testEventServiceWork();
-    expect(result).to.be.an('array')
-    expect(sent).to.be.an('boolean')
-    expect(sent).to.equal(true);  });
-
-  it('send mail service test', async () => {
-    const result = index.testSendEmailServiceWork();
-    expect(result).to.equal(true);
-  });
 });
