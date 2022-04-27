6e56a23680f1ea75d2932f17e7b58c0e320ab6a6
rans <Galeaf11@gmail.com>
2022-04-27 19:50:09 +0600

    Get types from data-models package

--- packages/watcher/src/types.ts
+++ packages/watcher/src/types.ts
@@ -1,83 +1,4 @@
-import type {
-  MediaListReference,
-  AddressReference,
-  MessengerReference
-} from '@windingtree/org.json-schema/types/org.json';
-import {
-  allowedSpaceTypes,
-  allowedLodgingFacilityTypes,
-  allowedLodgingFacilityTiers
-} from './enum';
-
-export type SpaceType =
-	typeof allowedSpaceTypes[number];
-
-export type LodgingFacilityType =
-	typeof allowedLodgingFacilityTypes[number];
-
-export type LodgingFacilityTier =
-	typeof allowedLodgingFacilityTiers[number];
-
-export type Amenity = string;
-
-export interface FacilityContactReference {
-  name: string;
-  phone: string;
-  email: string;
-  website: string;
-  messengers?: MessengerReference[];
-}
-
-export interface SpaceRaw {
-  name: string;
-  description: string;
-  amenities: Amenity[];
-  capacity: number;
-  guestsNumber: number;
-  beds: number;
-  price: string;
-  type: SpaceType;
-  media: MediaListReference;
-}
-
-export interface LodgingFacilityRaw {
-  name: string;
-  description: string;
-  type: LodgingFacilityType;
-  tier: LodgingFacilityTier;
-  amenities: Amenity[];
-  contact: FacilityContactReference;
-  address: AddressReference;
-  what3words?: string;
-  operator: {
-    name: string;
-    address: AddressReference;
-  }
-  media: MediaListReference;
-}
-
-export interface Space extends SpaceRaw {
-  contractData: {
-    spaceId: string;
-    active: boolean;
-    lodgingFacilityId: string;
-    capacity: number;
-    pricePerNightWei: string;
-    dataURI: string;
-  }
-  updated: string;
-}
-
-export interface LodgingFacility extends LodgingFacilityRaw {
-  contractData: {
-    lodgingFacilityId: string;
-    active: boolean;
-    owner: string;
-    dataURI: string;
-  },
-  updated: string;
-  spaces: Space[];
-}
+import { LodgingFacility, Space } from 'stays-data-models'
 
 export interface TokenEntity {
   tokenId: string,
@@ -95,4 +16,3 @@ export interface TokenEntity {
   facility?: LodgingFacility,
   space?: Space
 }
-
