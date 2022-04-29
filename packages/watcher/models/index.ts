6eb0aba9169b397b6ff36f2eca0b6367479be1d2
rans <Galeaf11@gmail.com>
2022-04-29 19:38:57 +0600

    remove from .env db pass and db login

--- packages/watcher/models/index.ts
+++ packages/watcher/models/index.ts
@@ -5,11 +5,8 @@ import {Sequelize} from 'sequelize';
 dotenv.config();
 
 const db_host = process.env.DB_HOST || 'development.db';
-const db_login = process.env.DB_LOGIN || '';
-const db_pass = process.env.DB_PASSWORD || '';
 
-
-export const sequelize = new Sequelize('sqlite', db_login, db_pass, {
+export const sequelize = new Sequelize('sqlite', '', 'db_pass', {
   dialect: 'sqlite',
   host: db_host,
 });
